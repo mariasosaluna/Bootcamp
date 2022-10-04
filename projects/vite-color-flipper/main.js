8a83bfbef5e93d07b7de176d3af36e375a0b06cc
mariasosaluna <maria.sosa.luna@gmail.com>
2022-10-04 18:43:57 +0200

    add vite-color-flipper basic project

---
+++ projects/vite-color-flipper/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+const COLOR_PALETTE = {
+  '#A2FAA3': 'Mint Green',
+  '#92C9B1': 'Turquoise Green',
+  '#4F759B': 'Blue Yonder',
+  '#5D5179': 'Purple Navy',
+  '#571F4E': 'Palatinate Purple'
+}
---
+++ projects/vite-color-flipper/modules.js
@@ -0,0 +1,12 @@
+export const person = {
+    name: "Stephen",
+    surname: "Strange"
+};
+
+export const sayHello = () => {
+    console.log("Hello!");
+};
+
+const DEV_LAN = "Javascript";
+export default DEV_LAN;
+
