cf50c7cde7c64a37240061d22dbcac84854e9026
mariasosaluna <maria.sosa.luna@gmail.com>
2022-10-04 20:59:04 +0200

    iteracion#1

--- projects/vite-color-flipper/main.js
+++ projects/vite-color-flipper/main.js
@@ -1,9 +1,28 @@
-import './style.css'
+import './style.css';
 
-const COLOR_PALETTE = {
-  '#A2FAA3': 'Mint Green',
-  '#92C9B1': 'Turquoise Green',
-  '#4F759B': 'Blue Yonder',
-  '#5D5179': 'Purple Navy',
-  '#571F4E': 'Palatinate Purple'
+const COLOR_PALETTE = [
+  {color: '#A2FAA3', name: 'Mint Green'},
+  {color: '#92C9B1', name: 'Turquoise Green'},
+  {color: '#4F759B', name: 'Blue Yonder'},
+  {color: '#5D5179', name: 'Purple Navy'},
+  {color: '#571F4E', name: 'Palatinate Purple'},
+];
+
+const addButtonToColorPicker = () => {
+  let buttonContainer = document.querySelector(".block");
+
+  COLOR_PALETTE.forEach((color) => {
+    let button = document.createElement('button');
+    button.innerHTML = color.name;
+    button.style.backgroundColor = color.color;
+
+    button.addEventListener("click", (event) => {
+      document.body.style.backgroundColor = color.color;
+    });
+
+    buttonContainer.appendChild(button);
+    console.log(color);
+  });
 }
+
+addButtonToColorPicker();
